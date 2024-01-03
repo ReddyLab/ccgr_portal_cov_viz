1f79a8dc680724dce969548a6d0a1dfdcd1c37af
Thomas Cowart <thomas.cowart@duke.edu>
2024-01-03 13:09:34 -0500

    Adds more set ops to SetOp
    Also had to make some parameters references because they will be called from a reference upstream.

--- exp_viz/src/set_ops.rs
+++ exp_viz/src/set_ops.rs
@@ -1,28 +1,31 @@
 use cov_viz_ds::ExperimentFeatureData;
 use std::boxed::Box;
 
+#[derive(Clone, Debug)]
 pub struct FeatureDataValue {
     pub feature_data: Option<ExperimentFeatureData>,
     pub feature_data_set: Option<Box<SetOp>>,
 }
 
 impl FeatureDataValue {
-    pub fn data_set(self) -> Option<ExperimentFeatureData> {
-        if let Some(feature_data) = self.feature_data {
-            return Some(feature_data.to_owned());
-        } else if let Some(set_op) = self.feature_data_set {
-            return set_op.merge();
+    pub fn data_set(&self) -> Option<ExperimentFeatureData> {
+        if self.feature_data.is_some() {
+            return self.feature_data.clone();
+        } else if let Some(set_op) = &self.feature_data_set {
+            return set_op.coalesce();
         }
 
         return None;
     }
 }
 
+#[derive(Clone, Debug)]
 pub enum Op {
     Intersection,
     Union,
 }
 
+#[derive(Clone, Debug)]
 pub struct SetOp {
     pub op: Op,
     pub left: FeatureDataValue,
@@ -30,7 +33,7 @@ pub struct SetOp {
 }
 
 impl SetOp {
-    pub fn merge(self) -> Option<ExperimentFeatureData> {
+    pub fn coalesce(&self) -> Option<ExperimentFeatureData> {
         match (self.left.data_set(), self.right.data_set()) {
             (Some(left), Some(right)) => match self.op {
                 Op::Intersection => Some(left.intersection(&right)),
